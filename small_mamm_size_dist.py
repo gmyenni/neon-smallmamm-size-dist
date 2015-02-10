7b5d4dce69409aa0242b7e8059a9535f0e293881
Ethan White <ethan@weecology.org>
2015-02-10 11:41:10 -0700

    Import data

---
+++ small_mamm_size_dist.py
@@ -0,0 +1,14 @@
+import os
+import pandas as pd
+from into import into
+from glob import glob
+
+def import_data():
+    datafiles = glob('data/NEON.*.csv')
+    data = pd.DataFrame()
+    for datafile in datafiles:
+        new_data = pd.read_csv(datafile)
+        data = data.append(new_data, ignore_index=True)
+    return data
+
+data = import_data()
