5d2dc361708ec336fb870e3e6da8688be5c9a073
Ethan White <ethan@weecology.org>
2015-02-10 12:01:02 -0700

    Make histograms of weight for each site

--- small_mamm_size_dist.py
+++ small_mamm_size_dist.py
@@ -1,8 +1,11 @@
 import pandas as pd
 from into import into
 from glob import glob
+import numpy as np
+import matplotlib.pyplot as plt
 
 def import_data():
+    """Import data files from the ./data directory"""
     datafiles = glob('data/NEON.*.csv')
     data = pd.DataFrame()
     for datafile in datafiles:
@@ -10,4 +13,13 @@ def import_data():
         data = data.append(new_data, ignore_index=True)
     return data
 
+def make_size_dist(weights):
+    """Make a histogram representing the individual size distribution"""
+    weights = weights.dropna().values
+    plt.hist(np.log(weights))
+
 data = import_data()
+for i, (site, site_data) in enumerate(data.groupby('siteID')):
+    plt.subplot(3, 4, i)
+    make_size_dist(site_data['weight'])
+plt.show()
